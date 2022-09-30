5660102b347e5bda300e4e2f401782d1698f0e8b
Jack Coleman <jackcoleman@Jacks-MacBook-Pro-2.local>
2022-09-30 15:42:39 -0400

    test

--- lib/facter/bitvise_version.rb
+++ lib/facter/bitvise_version.rb
@@ -10,6 +10,11 @@ Facter.add('bitvise_version') do
     end
     cfg_object = keys.select { |i| i[%r{^\w+\.\w+$}] }.select { |i| i[%r{BssCfg}] }[0]
     cfg = WIN32OLE.new(cfg_object)
-    cfg.version.buildVersion
+    version = cfg.version.buildVersion.split('.')
+    if version.length > 2
+      version.join('.')
+    else
+      "#{version.join('.')}.0"
+    end
   end
 end
